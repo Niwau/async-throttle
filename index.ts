15d71d5c9e8502d60d988e8c0ad2bffef05eb2f0
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-03-18 14:24:56 -0300

    feat: add throttle function

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+const delays = [...Array(50)].map(() => Math.floor(Math.random() * 900) + 100);
+
+const load = delays.map(
+    (delay) => (): Promise<number> =>
+        new Promise((resolve) => {
+            setTimeout(() => resolve(Math.floor(delay / 100)), delay);
+        })
+);
+
+type Task = () => Promise<number>;
+
+const throttle = async (workers: number, tasks: Task[]) => {
+    const answers: number[] = [];
+    for (let i = 0; i < tasks.length; i += workers) {
+        const results = await Promise.all(
+            tasks.slice(i, i + workers).map((task) => task())
+        );
+        answers.push(...results);
+    }
+    return answers;
+};
+
+const bootstrap = async () => {
+    console.log("Starting...");
+    const start = Date.now();
+    const answers = await throttle(5, load);
+    console.log("Done in %dms", Date.now() - start);
+    console.log("Answers: %O", answers);
+};
+
+bootstrap().catch((err) => {
+    console.log("General fail: %O", err);
+});
